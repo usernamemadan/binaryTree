ddee9c85e7c424cc17ea056326bd4b3839b75778
madan <madan@local.com>
2021-09-16 00:19:46 +0530

    added the given nodes into binary tree and size of tree is printed

--- src/com/binarytree/Main.java
+++ src/com/binarytree/Main.java
@@ -7,7 +7,18 @@ public class Main {
 		mybinaryTree.add(56);
 		mybinaryTree.add(30);
 		mybinaryTree.add(70);
+		mybinaryTree.add(22);
+		mybinaryTree.add(40);
+		mybinaryTree.add(11);
+		mybinaryTree.add(3);
+		mybinaryTree.add(16);
+		mybinaryTree.add(60);
+		mybinaryTree.add(95);
+		mybinaryTree.add(65);
+		mybinaryTree.add(63);
+		mybinaryTree.add(67);
 		
 		mybinaryTree.printNode();
+		System.out.println("\nsize of binary tree is " +mybinaryTree.getSize() );;
 	}
 }
